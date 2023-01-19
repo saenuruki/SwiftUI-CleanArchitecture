07fe2eaaf32849153340db6d9f873c2043dfe63a
Sae Nuruki <sae.webwork@gmail.com>
2023-01-19 11:17:45 -0500

    Create New Project

---
+++ SwiftUICleanArchitecture/SwiftUICleanArchitecture/SwiftUICleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureApp.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Sae Nuruki on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
